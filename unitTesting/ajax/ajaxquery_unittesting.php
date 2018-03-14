45fd832b8394dc20c7bdd0386d3c59353bd35402
Onur Yukselen <onuryukselen@gmail.com>
2018-03-14 11:16:26 -0400

    aaa

--- unitTesting/ajax/ajaxquery_unittesting.php
+++ unitTesting/ajax/ajaxquery_unittesting.php
@@ -11,31 +11,31 @@ use PHPUnit\Framework\TestCase;
 
 class ajaxQueryTest extends TestCase
 {
-    /**
-     * @beforeClass
-     */
-    public function testInsertProject() {
-		ob_start();
-		$_REQUEST['p'] = 'saveProject';
-		$_REQUEST['name'] = 'testProject';
-		$_REQUEST['summary'] = "testSummary";
-		include('ajaxquery.php');
-		$this->assertEquals(json_decode($data)->id,'2');
-		ob_end_clean();
-	}
-    /**
-     * @afterClass
-     */
-	public function testGetProjects() {
-		ob_start();
-		$_REQUEST['p'] = 'getProjects';
-		$_REQUEST['id'] = 2;
-		include('ajaxquery.php');
-		$this->assertEquals(json_decode($data)[0]->id,'2');
-		$this->assertEquals(json_decode($data)[0]->name,'testProject');
-		$this->assertEquals(json_decode($data)[0]->summary,'testSummary');
-		ob_end_clean();
-	}
+/**
+* @beforeClass
+*/
+public function testInsertProject() {
+ob_start();
+$_REQUEST['p'] = 'saveProject';
+$_REQUEST['name'] = 'testProject';
+$_REQUEST['summary'] = "testSummary";
+include('ajaxquery.php');
+$this->assertEquals(json_decode($data)->id,'2');
+ob_end_clean();
+}
+/**
+* @afterClass
+*/
+public function testGetProjects() {
+ob_start();
+$_REQUEST['p'] = 'getProjects';
+$_REQUEST['id'] = 2;
+include('ajaxquery.php');
+$this->assertEquals(json_decode($data)[0]->id,'2');
+$this->assertEquals(json_decode($data)[0]->name,'testProject');
+$this->assertEquals(json_decode($data)[0]->summary,'testSummary');
+ob_end_clean();
+}
 
 }
 
